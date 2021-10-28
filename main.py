68c2c56148e5597f24bbb00010b85c82d1a98e1f
NickM0864 <74526023+NickM0864@users.noreply.github.com>
2021-10-28 15:20:56 -0400

    updates

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ Listings_Feb_Jun = Housing_Data[['ListingsFreq_Feb', 'ListingsFreq_Mar',
                                     'ListingsFreq_Apr','ListingsFreq_May','ListingsFreq_Jun']].plot.box(vert = False)
 
 #Drop February and March data due to tiny sample size.
-# This shouldn't impact the analysis because there is only two weeks of overlap between the time period and the start of coronavirus in America.
+#This shouldn't impact the analysis because there is only two weeks of overlap between the time period and the start of coronavirus in America.
 #Also dropping town and county columns as they're no longer descriptive.
 Housing_Data[['ListingsFreq_Feb', 'ListingsFreq_Mar', 'ListingsFreq_Apr']].describe()
 Housing_Data.drop(columns = ['ListingsFreq_Feb', 'ListingsFreq_Mar',
@@ -36,7 +36,7 @@ Listings_by_Month = ['ListingsFreq_Apr','ListingsFreq_May','ListingsFreq_Jun',
 # Recalculating listing frequency after removing Feb/March
 Housing_Data["ListingsFreq_Cross"]= Housing_Data[Listings_by_Month].sum(axis = 1)
 Housing_Data["ListingsFreq_Cross"].describe
-# Removing all neighborhoods with fewer than 125 listings. Reduced neighborhoods from 200 to 65.
+# Removing all neighborhoods with fewer than 125 listings. Reduced neighborhood count from 200 to 65.
 Housing_Data = Housing_Data[Housing_Data['ListingsFreq_Cross'] > 125]
 
 
@@ -45,7 +45,7 @@ Listings_Apr_Dec = Housing_Data[['ListingsFreq_Apr', 'ListingsFreq_May', 'Listin
                      'ListingsFreq_Oct','ListingsFreq_Nov',"ListingsFreq_Dec"]].plot.box(vert = False)
 
 # Recalculating neighborhood mean rent after removing Feb/March.
-# Removing February and March median rent data did not change the statistical averages significantly.
+# Removing February and March median rent data did not significantly change the averages.
 Housing_Data["MeanRent_Cross"].describe()
 Mean_Rent_by_Month = Housing_Data[["MeanRent_Apr", "MeanRent_May", "MeanRent_Jun", "MeanRent_Jul",
                         "MeanRent_Aug","MeanRent_Sep","MeanRent_Oct","MeanRent_Nov","MeanRent_Dec"]]
@@ -56,13 +56,12 @@ Housing_Data.reset_index(inplace = True, drop = True)
 
 #Calculate the percentage of neighborhoods with fewer than 5 listings per month, sorted by month.
 #We see a general trend of fewer neighborhoods hitting the 5 listings per month threshold.
-# November and December levels are particularly high, with 11% and 17% respectively of neighborhoods below 5 listings per month.
-#Rental activity declines in the winter, so this most likely explains the general decline in listings.
+# November and December levels are particularly high, with 11% and 17% of neighborhoods below 5 listings per month.
+#Rental activity declines in the winter, likely explaining the decline in listings.
 listings_threshold = Housing_Data[Listings_by_Month][Housing_Data[Listings_by_Month] > 5]
 percent_missing = listings_threshold.isnull().sum() * 100 / len(listings_threshold)
 percent_missing
-#To reduce the rental median variance, I remove 4 neighborhoods that /
-# fall below the monthly listing thresholds for two distinct months.
+#To reduce the rental median variance, removing 4 neighborhoods that fall below the monthly listing thresholds for two distinct months.
 threshold_mask = ~pd.isnull(listings_threshold)
 threshold_count = np.apply_along_axis(np.count_nonzero, 1, threshold_mask)
 Housing_Data['Count'] = threshold_count
@@ -71,7 +70,7 @@ Housing_Data = Housing_Data[Housing_Data['Count'] > 7]
 Housing_Data['Count']
 
 
-#For the 61 neighborhoods that pass the overall threshold count, I null the rental-mean data for months with fewer than 5 listings.
+#For the 61 neighborhoods that pass the overall threshold count, nulling the rental mean data for months with fewer than 5 listings.
 rent_month_list = ["MeanRent_Apr", "MeanRent_May", "MeanRent_Jun", "MeanRent_Jul",
                   "MeanRent_Aug","MeanRent_Sep","MeanRent_Oct","MeanRent_Nov","MeanRent_Dec"]
 column_name_zip = dict(zip(Listings_by_Month, rent_month_list))
@@ -103,6 +102,7 @@ Clean_Housing_Data['Oct-Dec_Mean'] = Clean_Housing_Data.iloc[:,18:21].mean(axis 
 Clean_Housing_Data['Apr-Jun_Mean'] /= 1.02
 Clean_Housing_Data['Jul-Sep_Mean'] /= 1.02
 
+#Splitting the month range into two subcategories for more granular analysis
 Clean_Housing_Data['First Delta'] = ((Clean_Housing_Data['Apr-Jun_Mean'] - Clean_Housing_Data['Jul-Sep_Mean']) / Clean_Housing_Data['Apr-Jun_Mean']) * 100
 Clean_Housing_Data['Second Delta'] = ((Clean_Housing_Data['Jul-Sep_Mean'] - Clean_Housing_Data['Oct-Dec_Mean']) / Clean_Housing_Data['Jul-Sep_Mean']) * 100
 
@@ -111,12 +111,13 @@ print(Clean_Housing_Data['First Delta'].describe())
 print(Clean_Housing_Data['Second Delta'].describe())
 
 Clean_Housing_Data.reset_index(inplace = True, drop = True)
-    # In order to plot the neighborhood data over Boston, I convert merge the dataset with an existing geopandas frame that contains the coordinates of each neighborhood. \\
-    #Then convert the frame into JSON for use in Plotly.
+# In order to plot the neighborhood data over Boston, I convert merge the dataset with an existing geopandas frame that contains the coordinates of each neighborhood. \\
+#Then convert the frame into JSON for use in Plotly.
 Geo_Housing_Data = Geohousing.merge(Clean_Housing_Data, how = 'left',
                                            left_on = 'CTID1', right_on = 'CT_ID_10')
 
 Geo_Housing_Data.drop(columns = ['CTID1','MeanRent_Diff'], axis = 1, inplace = True)
+#Replacing NA with zero so all neighborhoods appear on chloropleth map
 Geo_Housing_Data[['CT_ID_10', 'ListingsFreq_Cross', 'ListingsFreq_Apr', 'ListingsFreq_May', 'ListingsFreq_Jun',
                   'ListingsFreq_Jul', 'ListingsFreq_Aug', 'ListingsFreq_Sep',
                   'ListingsFreq_Oct', 'ListingsFreq_Nov', "ListingsFreq_Dec","First Delta","Second Delta"]] = Geo_Housing_Data[['CT_ID_10','ListingsFreq_Cross', 'ListingsFreq_Apr', 'ListingsFreq_May', 'ListingsFreq_Jun',
--- median_income_scraper.py
+++ median_income_scraper.py
@@ -1,39 +1,38 @@
-import time
 import numpy as np
 import pandas as pd
-
+import os
 from geo import Geohousing
 import requests
-import json
 
 dcid_list = Geohousing['CTID1']
-
 median_income_dict = dict()
 
-for id in dcid_list:
+#Run scraper if CSV doesn't exist
+file_path_str = 'median_income.csv' #Relative path
+if not os.path.exists(file_path_str):
+    for id in dcid_list:
+        median_income_get = requests.get(url = f'https://api.datacommons.org/stat/value?place=geoId/{id}&stat_var=Median_Income_Household')
+        median_income_str = median_income_get.text
+        median_income_dict[id] = median_income_str
+else:
+    median_income_df = pd.DataFrame.from_dict(median_income_dict, orient = 'index').reset_index()
+    median_income_df.columns = ['CT_ID', 'Median_Income']
+    median_income_df['Median_Income'] = median_income_df['Median_Income'].str.extract('(\d+)').astype(int)
+    format_dict = {'Median_Income':'${0:,.0f}'}
+    median_income_df.style.format(format_dict)
+    #print(median_income_df['Median_Income'].value_counts())
+    median_income_df[median_income_df['Median_Income'] == 5] = pd.NA
+    print(median_income_df['Median_Income'].isna().sum())
+    median_income_df['Median_Income_Percentile_Rank'] = median_income_df['Median_Income'].rank(pct = True).round(2)
+    median_income_df['Median_Income_Percentile_Rank'] = pd.Series(["{0:.2f}%".format(val * 100) for val
+                                                                   in median_income_df['Median_Income_Percentile_Rank']],
+                                                                  index = median_income_df.index)
+    # median_income_df['C'] = np.where(
+    #     df['A'] == df['B'], 0, np.where(
+    #     df['A'] >  df['B'], 1, -1))
 
-    median_income_get = requests.get(url = f'https://api.datacommons.org/stat/value?place=geoId/{id}&stat_var=Median_Income_Household')
-    median_income_str = median_income_get.text
-    median_income_dict[id] = median_income_str
-
-median_income_df = (pd.DataFrame.from_dict(median_income_dict, orient = 'index').reset_index())
-median_income_df.columns = ['CT_ID', 'Median_Income']
-median_income_df['Median_Income'] = median_income_df['Median_Income'].str.extract('(\d+)').astype(int)
-format_dict = {'Median_Income':'${0:,.0f}'}
-median_income_df.style.format(format_dict)
-print(median_income_df['Median_Income'].value_counts())
-median_income_df[median_income_df['Median_Income'] == 5] = pd.NA
-print(median_income_df['Median_Income'].isna().sum())
-median_income_df['Median_Income_Percentile_Rank'] = median_income_df['Median_Income'].rank(pct = True).round(2)
-median_income_df['Median_Income_Percentile_Rank'] = pd.Series(["{0:.2f}%".format(val * 100) for val
-                                                               in median_income_df['Median_Income_Percentile_Rank']],
-                                                              index = median_income_df.index)
-median_income_df['C'] = np.where(
-    df['A'] == df['B'], 0, np.where(
-    df['A'] >  df['B'], 1, -1))
-
-print(median_income_df)
-median_income_csv = median_income_df.to_csv('~/PycharmProjects/pythonProject/median_income.csv', index = True)
+    print(median_income_df)
+    median_income_csv = median_income_df.to_csv('~/PycharmProjects/pythonProject/median_income.csv', index = True)
 
 
 
